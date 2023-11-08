f32a2def25303e8a094c4b2257d2e49f36b2794e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-11-08 17:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+function modelBody(){
+
+	let open = document.getElementById("openModal");
+		let modal = document.querySelector(".modal");
+        let close = document.querySelector(".close-modal");
+	
+	open.addEventListener("click", function(){
+		modal.style.display = "block";
+	});
+
+      modal.addEventListener("click", function(event) {
+		         if( event.target === modal)
+				 {
+					 modal.style.display = "none";
+				 }
+	  });
+
+
+	close.addEventListener("click", function(){
+		modal.style.display = "none";
+	});
+
+}
+modelBody();
